c78a259230e3bf9f7e4bc8caa6d339c0b218f4a6
bhohnco <bryanhohn13@gmail.com>
2021-05-10 15:43:25 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
+//added for first commit
+
 ReactDOM.render(<App />, document.getElementById('root'));
